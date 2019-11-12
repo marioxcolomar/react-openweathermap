85950ea1f643e87007327bdd325fb70c0fdcaa45
marioxcolomar <marioxcolomar@gmail.com>
2019-11-12 11:28:55 +0100

    Remove footer and fix class name on weather component.

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -2,13 +2,12 @@ import React from 'react'
 
 const Weather = props => {
   return (
-    <div className="main">
+    <div className="weather">
       <p><b>Wind speed: </b>{props.wind} m/s</p>
       <p><b>Temperature: </b>{props.temp}&#xb0;</p>
       <p><b>Highest temperature: </b>{props.temp_max}&#xb0;</p>
       <p><b>Lowest emperature: </b>{props.temp_min}&#xb0;</p>
       <p><b>Humidity: </b>{props.humidity} %</p>
-      <p><b>Last update: </b>{props.timestamp}</p>
     </div>
   );
 }
