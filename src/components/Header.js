15f024343a44aa5d1ac139cf587bbee0f9d95735
marioxcolomar <marioxcolomar@gmail.com>
2019-11-12 11:28:20 +0100

    Simplify header to only include city and country.

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = (props) => {
     return (
-        <h1>Weather in {props.city}, {props.country}</h1>
+        <h1>{props.city}, {props.country}</h1>
     )
 }
 
