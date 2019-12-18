fcc873a828a743d4cbe1c85bb39eaf6c3ce135e3
marioxcolomar <marioxcolomar@gmail.com>
2019-12-18 17:30:31 +0100

    Rename App component and use new controller.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,16 +1,16 @@
 import React, { Component } from 'react'
 import '../App.css'
 import timestamp from 'time-stamp'
+import getWeather from '../controllers/openweather'
 
 //Components
 import Weather from './Weather'
 import Header from './Header';
 import Footer from './Footer'
 
-let apiKey = '254d2233a505bdf1e8e64ea5b4ed6538'
 let apiUrl =
   '//api.openweathermap.org/data/2.5/weather?q=Copenhagen,dk&units=metric&appid=' +
-  apiKey
+  process.env.REACT_APP_OW_API_KEY
 
 class App extends Component {
   state = {
@@ -24,27 +24,13 @@ class App extends Component {
     country: null
   }
 
-  fetchWeatherData = () => {
-    fetch(apiUrl)
-      .then(res => {
-        if (res >= 400 || !res.ok) {
-          throw new Error("Server failed to respond")
-        }
-        return res.json()
-      })
-      .then(data => {
-        this.setState({
-          wind: data.wind.speed,
-          temp: data.main.temp,
-          humidity: data.main.humidity,
-          temp_min: data.main.temp_min,
-          temp_max: data.main.temp_max,
-          timestamp: timestamp("HH:mm:ss"),
-          city: data.name,
-          country: data.sys.country
-        })
-      })
-  }
+  getWeather<>(apiUrl)
+    .then({data} => {
+      console.log('data',data)
+    })
+    .catch(error => {
+      console.log('Error: ', error)
+    })
 
 
   componentDidMount() {
